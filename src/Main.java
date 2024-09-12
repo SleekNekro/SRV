577378fd8d4572e5a560f815b2c56295626184f3
ikepermat2 <ikerperezmata03@gmail.com>
2024-09-12 12:08:42 +0200

    ejer a por terminar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+class Alumne extends Persona{}
+class Persona {}
+
 public class Main {
+    static void imprimirJerarquiaDeClasses(Class<?> c) {
+        System.out.print(c.getName() + (c.getSuperclass() != null ? "  >  " : "\n"));
+        if (c.getSuperclass() == null) return;
+        imprimirJerarquiaDeClasses(c.getSuperclass());
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
